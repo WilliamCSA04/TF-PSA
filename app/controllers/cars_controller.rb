57125765c345b0c606983dcedb4e8b23dac58ca7
williamcsa04 <williamcaetano14@gmail.com>
2016-11-26 19:47:19 -0200

    Fixxed wrong logic at update method at cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -17,13 +17,8 @@ class CarsController < ApplicationController
 
   def update
     begin
-      raise "Full type is empty" if :full_type.empty?
-      raise "Plate is empty" if :plate.empty?
-      raise "Model is empty" if :model.empty?
-      raise "Year is too old" if :year < 1884
-      raise "Fuel capacity should be higher than 0" unless :fuel_capacity > 0
-      raise "Manufacturer is empty" if :manufacturer.empty?
-      @car = car.update_attributes(params)
+      @car = Car.find(:car_id)
+      @car.update_attributes(params)
       @car.save_car
     rescue => e.message
       render(json: {error: e.message}, status: :unprocessable_entity)
