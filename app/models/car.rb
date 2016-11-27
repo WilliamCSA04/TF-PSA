60699574c238ad67a09795c52944c83f46f1c8a7
williamcsa04 <williamcaetano14@gmail.com>
2016-11-27 21:02:27 -0200

    Added relashionship between models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,7 @@
 class Car < ActiveRecord::Base
 
+has_many :supplies
+
   def self.get_all_cars
     Car.all
   end
--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,5 +1,9 @@
 class Supply < ActiveRecord::Base
 
+has_one :serie
+has_one :car
+
+
   def self.get_all_supplies
     Supply.all
   end
