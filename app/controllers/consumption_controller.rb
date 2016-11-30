fb5ec97d27f80f6f2cd55af678d1059e4ae1e438
Raphael Braun <mbraun.raphael@gmail.com>
2016-11-30 18:24:49 -0200

    Case insensitive on plate search

--- app/controllers/consumption_controller.rb
+++ app/controllers/consumption_controller.rb
@@ -5,7 +5,7 @@ class ConsumptionController < ApplicationController
     begin
       search = params[:search_field]
       raise "search field is null" if search.nil?
-      @car = Car.where(plate: search).first
+      @car = Car.where(plate: search.upcase).first
       raise "no car with this plate" if @car.nil?
       @supplies = Supply.where(car_id: @car.id).order(series_id: :desc)
       @series_hash = @car.get_all_car_statistics
