8cd9f32d285c34f91f06da1200f1567ccbca25b8
williamcsa04 <williamcaetano14@gmail.com>
2016-11-30 03:32:11 -0200

    Primary version to generate consume report created

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,4 +6,42 @@ has_many :supplies
     Car.all
   end
 
+  def get_all_car_statistics
+    supplies = Supply.where(car_id: self.id).order(created_at: :desc)
+    return [] if supplies.nil?
+    series_hash = {}
+    actual_serie = supplies[0].series_id
+    total_cost = 0
+    min_serie_distance = 10000000000000
+    max_serie_distance = -1
+    total_liter = 0
+    supplies.each do |s|
+      element_serie = s.series_id
+      element_distance = s.odometer_from_car
+
+      if element_serie != actual_serie
+        total_distance = max_serie_distance - min_serie_distance
+        min_serie_distance = 10000000000000
+        max_serie_distance = -1
+        values = [total_liter, total_distance, total_cost]
+        series_hash[actual_serie] = values
+        actual_serie = element_serie
+      end
+
+      if element_distance < min_serie_distance
+        min_serie_distance = element_distance
+      elsif element_distance > max_serie_distance
+        max_serie_distance = element_distance
+      end
+      total_liter += total_liter + (s.total_cost / s.cost_liter)
+      total_cost += total_cost + s.total_cost
+    end
+
+    total_distance = max_serie_distance - min_serie_distance
+    values = [total_liter, total_distance, total_cost]
+    series_hash[actual_serie] = values
+    series_hash
+
+  end
+
 end
