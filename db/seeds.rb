1bbe659dde69113ec2098e945e4fb2b4a29c7ae0
williamcsa04 <williamcaetano14@gmail.com>
2016-11-30 03:52:24 -0200

    Added some seeds to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Serie.create
+  Serie.create
+  Serie.create
+  Serie.create
+  Serie.create
+  Serie.create
+
+  Car.create(fuel_type: 'Gasolina normal', plate: '1A2B3C', model: 'teste', year: '1999', fuel_capacity: 100, manufacturer: 'teste')
+  Car.create(fuel_type: 'Gasolina normal', plate: '2A2B3C', model: 'teste', year: '1999', fuel_capacity: 100, manufacturer: 'teste')
+  Car.create(fuel_type: 'Gasolina normal', plate: '3A2B3C', model: 'teste', year: '1999', fuel_capacity: 100, manufacturer: 'teste')
+  Car.create(fuel_type: 'Gasolina normal', plate: '4A2B3C', model: 'teste', year: '1999', fuel_capacity: 100, manufacturer: 'teste')
+
+  Supply.create(fuel_type: 'Gasolina normal', odometer_from_car: 0, fuel_quantity: 50, total_cost: 100, cost_liter: 2, series_id: 1, car_id: 1)
+  Supply.create(fuel_type: 'Gasolina normal', odometer_from_car: 0, fuel_quantity: 50, total_cost: 100, cost_liter: 2, series_id: 1, car_id: 1)
+  Supply.create(fuel_type: 'Gasolina normal', odometer_from_car: 0, fuel_quantity: 50, total_cost: 100, cost_liter: 2, series_id: 2, car_id: 1)
+
+  # t.string   "fuel_type",         null: false
+  # t.integer  "odometer_from_car", null: false
+  # t.integer  "fuel_quantity",     null: false
+  # t.integer  "total_cost",        null: false
+  # t.integer  "cost_liter",        null: false
+  # t.integer  "series_id",         null: false
+  # t.integer  "car_id",            null: false
+  # t.datetime "created_at"
+  # t.datetime "updated_at"
